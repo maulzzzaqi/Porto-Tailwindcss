2b50fde10df376ee1bed5b4a492dee7d99e931b8
MaulzzZaqi <madezaqi2006@gmail.com>
2022-12-08 21:40:02 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const hamburger = document.querySelector("#hamburger");
+const menu = document.querySelector("#menu");
+
+hamburger.addEventListener("click", ()=>{
+    menu.classList.toggle("hidden")
+    hamburger.classList.toggle("bg-white")
+})
+
+hLinks.forEach(link=>{
+    link.addEventListener("click", ()=>{
+      menu.classList.toggle("hidden")
+      hamburger.classList.toggle("bg-white")
+    })
+  })
+  
+  moon.addEventListener("click", ()=>{
+    body.classList.toggle("dark")
+  })
